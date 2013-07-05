143ef71996f59219bbee20900763f07a04226cde
Chris T <chris@komposta.net>
2013-07-05 15:08:12 +0300

    Updated grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,41 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		// Metadata.
 		pkg: grunt.file.readJSON('navgoco.jquery.json'),
-		banner: '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-				'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-				' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+		banner: '/*\n' +
+				' * jQuery <%= pkg.title || pkg.name %> Plugin v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)\n' +
+				' * https://github.com/tefra/navgoco\n' +
+				' *\n' +
+				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+				' * <%= pkg.licenses[0].type %> - <%= pkg.licenses[0].url %>\n' +
+				' */\n',
 		// Task configuration.
 		clean: {
 			files: ['dist']
 		},
-		uglify: {
+		concat: {
 			options: {
-				banner: '<%= banner %>'
+				banner: '<%= banner %>',
+				stripBanners: true
 			},
 			dist: {
-				src: 'src/jquery.navgoco.js',
-				dest: 'src/jquery.navgoco.min.js'
+				src: ['src/jquery.navgoco.js'],
+				dest: 'src/jquery.navgoco.js'
 			},
+		},
+		uglify: {
+			dist: {
+				files: {
+					'src/jquery.navgoco.min.js': ['src/jquery.navgoco.js'],
+				}
+			},
+			libs: {
+				options: {
+					preserveComments: 'some'
+				},
+				files: {
+					'src/jquery.cookie.min.js': ['src/jquery.cookie.js'],
+				}
+			}
 		},
 		jshint: {
 			gruntfile: {
@@ -51,11 +69,12 @@ module.exports = function(grunt) {
 	});
 
 	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task.
-	grunt.registerTask('default', ['clean', 'jshint', 'uglify']);
+	grunt.registerTask('default', ['clean', 'jshint', 'concat', 'uglify']);
 };
