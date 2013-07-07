eb7ead6c3805a61a9d0b35b978ac64259a932b0a
Chris T <chris@komposta.net>
2013-07-07 03:49:22 +0300

    Merged collapse and expand methods into public method toggle

--- src/jquery.navgoco.js
+++ src/jquery.navgoco.js
@@ -1,17 +1,18 @@
 /*
- * jQuery Navgoco Menus Plugin v0.1.0 (2013-07-05)
+ * jQuery Navgoco Menus Plugin v0.1.0 (2013-07-07)
  * https://github.com/tefra/navgoco
  *
  * Copyright (c) 2013 Chris T
  * BSD - https://github.com/tefra/navgoco/blob/master/LICENSE-BSD
  */
 (function($) {
+
 	"use strict";
 
 	var Plugin = function(el, options, idx) {
 		this.el = el;
 		this.$el = $(el);
-		this.opts = options;
+		this.options = options;
 		this.uuid = this.$el.attr('id') ? this.$el.attr('id') : idx;
 		this.open = {};
 		this.init();
@@ -21,16 +22,14 @@
 	Plugin.prototype = {
 		init: function() {
 			var self = this;
-			if (self.opts.save) {
-				self._load();
-			}
+			self._load();
 			self.$el.find('ul').each(function(idx) {
 				var sub = $(this);
-				sub.data('index', idx);
-				if (self.opts.save && self.open.hasOwnProperty(idx)) {
-					sub.parent().addClass(self.opts.openClass);
+				sub.attr('data-index', idx);
+				if (self.options.save && self.open.hasOwnProperty(idx)) {
+					sub.parent().addClass(self.options.openClass);
 					sub.show();
-				} else if (sub.parent().hasClass(self.opts.openClass)) {
+				} else if (sub.parent().hasClass(self.options.openClass)) {
 					sub.show();
 					this.open[idx] = 1;
 				} else {
@@ -38,90 +37,93 @@
 				}
 			});
 
-			var parents = self.$el.find("li:has(ul) > a");
-			if (self.opts.caret) {
-				parents.append(self.opts.caret);
+			var links = self.$el.find("li:has(ul) > a");
+			if (self.options.caret) {
+				links.append(self.options.caret);
 			}
-			parents.click(function(e) {
+			links.click(function(e) {
 				e.preventDefault();
 				var sub = $(this).next();
 				var isOpen = sub.is(":visible");
 				self._toggle(sub, !isOpen);
-				if (self.opts.save) {
-					self._save();
-				}
+				self._save();
 			});
 		},
 		_toggle: function(sub, open) {
 			var self = this;
-			var idx = sub.data('index');
-
+			var idx = sub.attr('data-index');
+			var parent = sub.parent();
 			if (open) {
-				sub.parent().addClass(self.opts.openClass);
-				sub.slideDown(self.opts.slide);
+				parent.addClass(self.options.openClass);
+				sub.slideDown(self.options.slide);
 				self.open[idx] = 1;
 
-				if (self.opts.accordion) {
+				if (self.options.accordion) {
+					var parents = parent.parents("ul");
 					var allowed = {};
 					allowed[idx] = 1;
-					sub.parent().parents("ul").each(function() {
-						var idx = $(this).data('index');
+					parents.each(function() {
+						var idx = $(this).attr('data-index');
 						allowed[idx] = 1;
 						self.open[idx] = 1;
 					});
 
 					self.$el.find("ul:visible").each(function() {
-						var menu = $(this);
-						if (!allowed.hasOwnProperty(menu.data('index'))) {
-							self._toggle(menu, false);
+						var sub = $(this);
+						var idx = sub.attr('data-index');
+						if (!allowed.hasOwnProperty(idx)) {
+							self._toggle(sub, false);
 						}
 					});
 				}
-
 			} else {
-				sub.parent().removeClass(self.opts.openClass);
-				sub.slideUp(self.opts.slide);
+				parent.removeClass(self.options.openClass);
+				sub.slideUp(self.options.slide);
 				this.open[idx] = 0;
 			}
 		},
-		_save: function() {
-			var save = {};
-			for (var key in this.open) {
-				if (this.open[key] === 1) {
-					save[key] = 1;
-				}
+		toggle: function(flag) {
+			var self = this;
+			var list = Array.prototype.slice.call(arguments, 1);
+			var only = {};
+
+			for (var i = 0; i < list.length; i++) {
+				only[list[i]] = true;
 			}
-			cookie[this.uuid] = save;
-			$.cookie(this.opts.cookie.name, JSON.stringify(cookie), this.opts.cookie);
+
+			self.$el.find('ul').each(function() {
+				var sub = $(this);
+				var idx = sub.attr('data-index');
+				if (!only.length || only.hasOwnProperty(idx)) {
+					self._toggle(sub, flag);
+					if (only.length === 1) {
+						return false;
+					}
+				}
+			});
+			self._save();
 		},
-		_resetCookie: function() {
-			cookie[this.uuid] = {};
+		_save: function() {
+			if (this.options.save) {
+				var save = {};
+				for (var key in this.open) {
+					if (this.open[key] === 1) {
+						save[key] = 1;
+					}
+				}
+				cookie[this.uuid] = save;
+				$.cookie(this.options.cookie.name, JSON.stringify(cookie), this.options.cookie);
+			}
 		},
 		_load: function() {
-			if (cookie === null) {
-				var data = $.cookie(this.opts.cookie.name);
-				cookie = (data) ? JSON.parse(data) : {};
-			}
-			this.open = cookie.hasOwnProperty(this.uuid) ? cookie[this.uuid] : {};
-		},
-		collapse: function() {
-			var self = this;
-			self.$el.find('ul:visible').each(function() {
-				self._toggle($(this), false);
-			});
-			if (self.opts.save) {
-				self._save();
+			if (this.options.save) {
+				if (cookie === null) {
+					var data = $.cookie(this.options.cookie.name);
+					cookie = (data) ? JSON.parse(data) : {};
+				}
+				this.open = cookie.hasOwnProperty(this.uuid) ? cookie[this.uuid] : {};
 			}
 		},
-		expand: function() {
-			var self = this;
-			self.$el.find('ul').each(function() {
-				self._toggle($(this), true);
-			});
-			if (self.opts.save) {
-				self._save();
-			}
-		}
 	};
 
 	var cookie = null;
@@ -142,8 +144,10 @@
 	};
 
 	$.fn.navgoco = function(options) {
-		var callback = typeof options === 'string';
-		if (!callback) {
+		var method = (typeof options === 'string' && options.charAt(0) !== '_');
+		if (method) {
+			var args = Array.prototype.slice.call(arguments, 1);
+		} else {
 			options = $.extend({}, defaults, options || {});
 			if (!$.cookie) {
 				options.save = false;
@@ -152,9 +156,9 @@
 		return this.each(function(idx) {
 			var $this = $(this);
 			var obj = $this.data('navgoco');
-			if (callback && obj) {
-				obj[options]();
-			} else if (!obj) {
+			if (method && obj) {
+				obj[options].apply(obj, args);
+			} else if (!obj && !method) {
 				$this.data('navgoco', new Plugin(this, options, idx));
 			}
 		});
